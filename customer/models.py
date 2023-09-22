d9ccd84df626c2d033d6df6ff5f9175dae745533
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-09-22 23:11:00 +0000

    Add Customer model

--- customer/models.py
+++ customer/models.py
@@ -7,3 +7,15 @@ class MealCategory(models.Model):
 
     def __str__(self):
         return self.name
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
