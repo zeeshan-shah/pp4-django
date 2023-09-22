d87b5c70c42184193251b741063758e379943f6d
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-09-22 23:20:15 +0000

    Add menu view

--- customer/views.py
+++ customer/views.py
@@ -6,3 +6,22 @@ from .forms import CustomerDetailsForm
 
 def home(request):
     return render(request, 'customer/base.html')
+
+def menu_view(request):
+    # Retrieve all dishes from the Dish model
+    dishes = Dish.objects.all()
+    
+    # Categorize dishes
+    starter_dishes = dishes.filter(categories__name='Starter')
+    main_course_dishes = dishes.filter(categories__name='Main Course')
+    dessert_dishes = dishes.filter(categories__name='Dessert')
+    drink_dishes = dishes.filter(categories__name='Drink')
+    
+    context = {
+        'starter_dishes': starter_dishes,
+        'main_course_dishes': main_course_dishes,
+        'dessert_dishes': dessert_dishes,
+        'drink_dishes': drink_dishes,
+    }
+    
+    return render(request, 'customer/menu.html', context)
