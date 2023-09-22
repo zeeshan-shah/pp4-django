28ec20487275a891b7a1f291368cf9e26e2c8e00
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-09-22 23:22:34 +0000

    Add customer register

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Customer, Dish, MealCategory, Order
 
-# Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'email', 'city', 'state')
