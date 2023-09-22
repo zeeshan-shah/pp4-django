c4c73e5ca6570e09a9db9d172321a0d28487c41a
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-09-22 23:11:54 +0000

    Add Order model

--- customer/models.py
+++ customer/models.py
@@ -29,3 +29,27 @@ class Dish(models.Model):
 
     def __str__(self):
         return self.name
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    items = models.ManyToManyField(Dish)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    # Fetch street, city, state, and zipcode from the related Customer instance
+    street = models.CharField(max_length=100, editable=False)
+    city = models.CharField(max_length=100, editable=False)
+    state = models.CharField(max_length=100, editable=False)
+    zipcode = models.CharField(max_length=10, editable=False)
+
+    def save(self, *args, **kwargs):
+        # Fetch street, city, state, and zipcode from the related Customer instance
+        self.street = self.name.street
+        self.city = self.name.city
+        self.state = self.name.state
+        self.zipcode = self.name.zipcode
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Order #{self.id} by {self.name}"
