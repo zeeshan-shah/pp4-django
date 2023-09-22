06153f2e3067b45ddc1f2a7fd4b9e50af418ea1f
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-09-22 23:21:08 +0000

    Add order view

--- customer/views.py
+++ customer/views.py
@@ -39,3 +39,58 @@ def customer_details_view(request):
         form = CustomerDetailsForm()
     
     return render(request, 'customer/customer_details_form.html', {'form': form})
+
+def order_view(request):
+    if request.method == 'POST':
+        selected_dish_ids = request.POST.getlist('selected_dishes')
+        
+        # Get the selected dishes
+        selected_dishes = Dish.objects.filter(pk__in=selected_dish_ids)
+        
+        # Calculate total price
+        total_price = sum(selected_dish.price for selected_dish in selected_dishes)
+        
+        if request.user.is_authenticated:
+            # If the user is authenticated, retrieve their customer details from the database
+            try:
+                customer = request.user.customer  # Assuming you have a one-to-one relationship between User and Customer
+            except Customer.DoesNotExist:
+                # Handle the case where the customer details don't exist for the user
+                return redirect('menu')  # Redirect to the menu page
+                
+        else:
+            # If the user is not authenticated, process the form data to create a new customer
+            form = CustomerDetailsForm(request.POST)
+            if form.is_valid():
+                # Create a new customer instance with the form data
+                customer = form.save(commit=False)
+                customer.user = request.user  # Associate the customer with the current user
+                customer.save()
+
+            else:
+                # Handle the case where the form data is invalid
+                return redirect('menu')  # Redirect to the menu page
+        
+        # Create an order
+        order = Order(
+            user=request.user,
+            name=customer,
+            total_price=total_price,
+            street=customer.street,
+            city=customer.city,
+            state=customer.state,
+            zipcode=customer.zipcode,
+        )
+        order.save()
+        
+        # Add selected dishes to the order
+        order.items.set(selected_dishes)
+        
+        # Redirect to order.html with order details
+        context = {
+            'order': order,
+            'selected_dishes': selected_dishes,
+        }
+        return render(request, 'customer/order.html', context)
+    
+    return redirect('menu')
