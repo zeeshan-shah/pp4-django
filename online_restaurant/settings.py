f5af63bab825b49877d3bd324734430335c23766
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-09-23 10:00:26 +0000

    Debug sets to false to check heroku deployment

--- online_restaurant/settings.py
+++ online_restaurant/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-zeeshanshah-pp4django-jxzidkjdzuz.ws-eu104.gitpod.io', 'pp4-django-43b291f3bcf7.herokuapp.com']
 
