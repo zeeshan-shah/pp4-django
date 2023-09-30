9613865c55b9cd3dd3d1f4532076322dd67b224b
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-09-30 17:42:20 +0000

    styforms)in): Format forms.py to comply with PEP 8 style guide using pycodest

--- customer/forms.py
+++ customer/forms.py
@@ -2,7 +2,9 @@
 from django import forms
 from .models import Customer
 
+
 class CustomerDetailsForm(forms.ModelForm):
     class Meta:
         model = Customer
-        fields = ['name', 'email', 'city', 'street', 'state', 'zipcode']  # Include 'email' field
+        fields = ['name', 'email', 'city', 'street',
+                  'state', 'zipcode']  # Include 'email' field
