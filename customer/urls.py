ec106f2b3d0942a7c299679f18206d83028f8968
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-09-30 17:43:44 +0000

    style(urls): Format urls.py to comply with PEP 8 style guide using pycodestyle

--- customer/urls.py
+++ customer/urls.py
@@ -7,11 +7,13 @@ urlpatterns = [
     path('menu/', views.menu_view, name='menu'),
     path('menu/<slug:slug>/', views.dish_detail_view, name='dish-detail'),
     path('order/', views.order_view, name='order'),
-    # path('update-selected-dishes/', views.update_selected_dishes, name='update-selected-dishes'),
-    path('delete/<int:dish_id>/', views.delete_selected_dishes, name='delete_selected_dishes'),
+    path('delete/<int:dish_id>/', views.delete_selected_dishes,
+         name='delete_selected_dishes'),
     path('cart/', views.cart_view, name='cart'),
-    path('order-confirmation/', views.order_confirmation_view, name='order-confirmation'),
-    path('customer-details/', views.customer_details_view, name='customer-details'),
+    path('order-confirmation/', views.order_confirmation_view,
+         name='order-confirmation'),
+    path('customer-details/', views.customer_details_view,
+         name='customer-details'),
     path('contact-us/', views.contact_us, name='contact-us'),
     path('customer-profile', views.customer_profile, name="customer-profile")
 ]
