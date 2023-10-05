937d8c4c8c228134371d27accdf0f9cfb5e7b078
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-10-05 13:15:29 +0000

    docs(fviews: Add docstrings to fviews.py

--- customer/views.py
+++ customer/views.py
@@ -9,10 +9,28 @@ from decimal import Decimal
 
 
 def home(request):
+    """
+    Render the home page.
+
+    Args:
+        request: The HTTP request.
+
+    Returns:
+        HttpResponse: The rendered home page.
+    """
     return render(request, 'customer/home.html')
 
 
 def menu_view(request):
+    """
+    Render the menu page with categorized dishes.
+
+    Args:
+        request: The HTTP request.
+
+    Returns:
+        HttpResponse: The rendered menu page with categorized dishes.
+    """
     # Retrieve all dishes from the Dish model
     dishes = Dish.objects.all()
 
@@ -39,14 +57,33 @@ def menu_view(request):
 
 
 def dish_detail_view(request, slug):
+    """
+    Render the detail view of a specific dish.
+
+    Args:
+        request: The HTTP request.
+        slug (str): The slug of the dish.
+
+    Returns:
+        HttpResponse: The rendered dish detail page.
+    """
     dish = get_object_or_404(Dish, slug=slug)
     return render(request, 'customer/dish_detail.html', {'dish': dish})
 
 
 @login_required
 def customer_details_view(request):
-    # Check if the user already has customer details
-    customer = get_object_or_404(Customer, user=request.user)
+    """
+    Render the customer details form.
+
+    Args:
+        request: The HTTP request.
+
+    Returns:
+        HttpResponse: The rendered customer details form page.
+    """
+    # Check if the user already has a customer profile
+    customer, created = Customer.objects.get_or_create(user=request.user)
 
     if request.method == 'POST':
         form = CustomerDetailsForm(request.POST, instance=customer)
@@ -64,6 +101,17 @@ def customer_details_view(request):
 
 @login_required
 def delete_selected_dishes(request, dish_id):
+    """
+    Delete a selected dish from the cart.
+
+    Args:
+        request: The HTTP request.
+        dish_id (int): The ID of the dish to be removed.
+
+    Returns:
+        HttpResponseRedirect: Redirects to the cart page
+        after removing the dish.
+    """
     # Retrieve the selected dish IDs from the session
     order_details = request.session.get('order_details', {})
     selected_dish_ids = order_details.get('selected_dishes', [])
@@ -86,6 +134,15 @@ def delete_selected_dishes(request, dish_id):
 
 
 def calculate_total_price(selected_dish_ids):
+    """
+    Calculate the total price of selected dishes.
+
+    Args:
+        selected_dish_ids (list): List of selected dish IDs.
+
+    Returns:
+        float: The total price of the selected dishes.
+    """
     # Retrieve the selected dishes and their prices
     selected_dishes = Dish.objects.filter(pk__in=selected_dish_ids)
     # Calculate the total price by summing the prices of selected dishes
@@ -97,12 +154,17 @@ def calculate_total_price(selected_dish_ids):
 
 @login_required
 def order_view(request):
+    """
+    Handle the ordering of selected dishes.
+
+    Args:
+        request: The HTTP request.
+
+    Returns:
+        HttpResponse: The rendered order.html page.
+    """
     if request.method == 'POST':
         selected_dish_ids = request.POST.getlist('selected_dishes')
-
-        # if not selected_dish_ids:
-        #     # If no dishes are selected, display an error message
-        #     return redirect('cart')  # Redirect back to the menu page
 
         # Get the selected dishes
         selected_dishes = Dish.objects.filter(pk__in=selected_dish_ids)
@@ -164,6 +226,15 @@ def order_view(request):
 
 @login_required
 def cart_view(request):
+    """
+    Render the cart view with selected dishes.
+
+    Args:
+        request: The HTTP request.
+
+    Returns:
+        HttpResponse: The rendered cart view page.
+    """
     order_details = request.session.get('order_details')
 
     if not order_details or not order_details.get('selected_dishes'):
@@ -222,6 +293,15 @@ def cart_view(request):
 
 @login_required
 def order_confirmation_view(request):
+    """
+    Render the order confirmation view with order details.
+
+    Args:
+        request: The HTTP request.
+
+    Returns:
+        HttpResponse: The rendered order confirmation view page.
+    """
     # Retrieve the order details from the session
     order_details = request.session.get('order_details')
 
@@ -265,7 +345,7 @@ def order_confirmation_view(request):
     # Clear the order details from the session
     request.session.pop('order_details', None)
 
-    # Redirect to order-confirmation.html with order details
+    # Render the order-confirmation.html template with order details
     context = {
         'order': order,
         'selected_dishes': selected_dishes,
@@ -275,6 +355,15 @@ def order_confirmation_view(request):
 
 
 def contact_us(request):
+    """
+    Handle contact form submissions.
+
+    Args:
+        request: The HTTP request.
+
+    Returns:
+        HttpResponse: The rendered contact form or a thank you page.
+    """
     if request.method == 'POST':
         sender_name = request.POST.get('name')
         messages.success(
@@ -292,6 +381,15 @@ def contact_us(request):
 
 @login_required
 def customer_profile(request):
+    """
+    Render the customer profile view.
+
+    Args:
+        request: The HTTP request.
+
+    Returns:
+        HttpResponse: The rendered customer profile view page.
+    """
     # Check if the user already has customer details
     customer = get_object_or_404(Customer, user=request.user)
     if request.method == 'POST':
